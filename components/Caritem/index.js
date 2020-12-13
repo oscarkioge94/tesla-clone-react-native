6491310f7333e8f81f58ff6e2a54940d9e36fcca
oscar Kioge <mainoscar94@gmail.com>
2020-12-13 13:20:42 +0300

    done with the buttons

--- components/Caritem/index.js
+++ components/Caritem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, ImageBackground } from 'react-native'
+import StyleButton from '../StyledButton/index'
 import styles from './style'
 
 const CarItem = () => {
@@ -13,6 +14,18 @@ const CarItem = () => {
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at $16,999</Text>
         </View>
+        <StyleButton  
+        type="primary"
+         content={'Custom Order'} 
+         onPress={() => {
+             console.warn('custom order was pressed ')
+         }}/>
+          <StyleButton  
+        type="secondary"
+         content={'Existing Inventory'} 
+         onPress={() => {
+             console.warn('Existing Inventory was pressed ')
+         }}/>
       </View>
     )
 }
---
+++ components/StyledButton/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    button:{
+        
+        height:40,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:20
+
+
+    },
+    container:{
+        width:'100%',
+        padding:10,
+
+
+    },
+    text:{
+        fontSize:12,
+        fontWeight:'500',
+        textTransform:'uppercase',
+
+
+    }
+    
+    
+})
+
+export default styles
